bb3ab19440d4aa7a9f730bf74cd23e3506e5118e
VaishnaviHonale <137199492+VaishnaviHonale@users.noreply.github.com>
2023-08-13 15:13:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 //your code here
+let touristSpots = ['Tajmahal', 'Victoria Memorial', 'The Virupaksha Temple'];
+let updatedList=arr.map((element)=>
+	{
+		let words=element.split(" ");
+		let updatedString=words.reduce((prev,current,index)=>{
+			if(current!="The" && current!="an" && current!="a"){
+				return prev+" "+current;
+			}
+			return prev;
+		}, "")
+		return updatedString;
+	});
+let mp={};
+updatedList.forEach((element,index)=>{
+	mp[element]=arr[index];
+});
+updatedList.sort();
+let findAns=updatedList.map(element)=>{
+	return mp[element];
+};
+
 
